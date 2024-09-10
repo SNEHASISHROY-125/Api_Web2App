a281718ee86d2130bc1a26d9a1ada31a2abde06a
Snehasish Roy <rsnehasish125@gmail.com>
2024-09-10 22:27:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -106,6 +106,6 @@ def delete_user(request: DUser , db: Session=Depends(get_db) ):
 
 
 # # Execute the startup event function when running with uvicorn
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
